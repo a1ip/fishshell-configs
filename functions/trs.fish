213880ddfe14d5913077125e1c32df2ce6eea8f9
Phil Rigovanov <phil@rigovanov.ru>
2014-03-26 01:55:56 +0400

    google translate modified

--- functions/trs.fish
+++ functions/trs.fish
@@ -1,3 +1,7 @@
 function trs --description 'Google translate'
-    translate \{$argv[1]\} $argv[2..(count $argv)]
+    if test (count $argv) -gt 1
+    	translate \{$argv[1]\} $argv[2..(count $argv)]   	
+    else
+    	translate $argv
+    end
 end
