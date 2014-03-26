5a7425e33469d84975e150e11c1cac2c9f94a4f7
Phil Rigovanov <phil@rigovanov.ru>
2014-03-26 15:17:33 +0400

    trs improved

--- functions/trs.fish
+++ functions/trs.fish
@@ -1,6 +1,6 @@
 function trs --description 'Google translate'
     if test (count $argv) -gt 1
-    	translate \{$argv[1]\} $argv[2..(count $argv)]   	
+    	translate \{$argv[1]\} $argv[2..(count $argv)] | gawk '{sub(/^{ /, "")};1' | gawk '{sub(/ }$/, "")};1'
     else
     	translate $argv
     end
