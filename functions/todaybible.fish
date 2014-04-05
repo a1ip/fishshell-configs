94e68f70b090a9fdb71a24de6b619f4d9d101b0c
Phil Rigovanov <phil@rigovanov.ru>
2014-04-05 17:04:06 +0400

    todaybible function improved

--- functions/todaybible.fish
+++ functions/todaybible.fish
@@ -1,3 +1,9 @@
-function todaybible 
-	command ruby -e 'puts ARGF.read.split(/\n\n\n/)[Time.now.month][/\n#{Time.now.day.to_s}.*\n/]' < .planchrono.txt
+function todaybible --description 'Shows verses to read by cronological plan'
+	if test (count $argv) -eq 0 
+		command ruby -e 'puts ARGF.read.split(/\n\n\n/)[Time.now.month][/\n#{Time.now.day.to_s}.*\n/]' < ~/.planchrono.txt
+	else if test (count $argv) -eq 1
+		command ruby -e "puts ARGF.read.split(/\n\n\n/)[$argv[1].to_i]" < ~/.planchrono.txt
+	else
+		command ruby -e "puts ARGF.read.split(/\n\n\n/)[$argv[2].to_i][/\n$argv[1].*\n/]" < ~/.planchrono.txt
+	end
 end
