6a48de50ca2c9a17aee52ad22ba19d3814ea69b5
Phil Rigovanov <phil@rigovanov.ru>
2014-04-25 01:13:07 +0400

    added rvm to octopresspush

--- functions/octopresspush.fish
+++ functions/octopresspush.fish
@@ -1,3 +1,3 @@
 function octopresspush --description 'Push and deploy octopress'
-  command rvm use 2.1.1;rvm gemset use octopress;rake generate;rake deploy;git push origin source
+  rvm use 2.1.1;rvm gemset use octopress;rake generate;rake deploy;git push origin source
 end
