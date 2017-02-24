92c47e620c055f6548d80873256ce140be2031d4
Phil Rigovanov <phil@rigovanov.ru>
2017-02-24 13:03:41 +0300

    fortune moved to fish_greeting

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -2,6 +2,7 @@ function fish_greeting -d "what's up, fish?"
     if math "$LINES >= 20 && $COLUMNS >= 80" > /dev/null
         bash (dirname (status -f))/archey.sh
     end
+    fortune myown
 end
 
 # function fish_greeting
